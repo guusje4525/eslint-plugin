45054b69d49137cf1538ab8026623923ecccc59e
guusje4525 <guusje4525@gmail.com>
2024-01-01 18:53:09 +1300

    1.0.17

--- dist/correct-store-name/index.js
+++ dist/correct-store-name/index.js
@@ -17,42 +17,44 @@ const myRule = {
     },
     create: (context) => ({
         Identifier: (node) => {
-            var _a, _b, _c, _d, _e, _f, _g, _h, _j;
+            var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m, _o, _p;
             // ex.
             // const store = useAuthStore()
             // Triggers when a const variable name is store
-            if (node.name === 'store') {
+            if (node.name === 'store' && 'parent' in node && 'id' in node.parent && 'parent' in node.parent.id && 'init' in node.parent.id.parent && 'callee' in node.parent.id.parent.init) {
                 // Get the name of the value we are trying to assign to store
-                const callee = node.parent.id.parent.init.callee.name;
+                const callee = (_e = (_d = (_c = (_b = (_a = node === null || node === void 0 ? void 0 : node.parent) === null || _a === void 0 ? void 0 : _a.id) === null || _b === void 0 ? void 0 : _b.parent) === null || _c === void 0 ? void 0 : _c.init) === null || _d === void 0 ? void 0 : _d.callee) === null || _e === void 0 ? void 0 : _e.name;
                 let newStoreName = '';
                 // If we are initializing a new store
-                if (callee === 'useStore') {
-                    newStoreName = (_j = (_h = (_g = (_f = (_e = (_d = (_c = (_b = (_a = node === null || node === void 0 ? void 0 : node.parent) === null || _a === void 0 ? void 0 : _a.id) === null || _b === void 0 ? void 0 : _b.parent) === null || _c === void 0 ? void 0 : _c.init) === null || _d === void 0 ? void 0 : _d.callee) === null || _e === void 0 ? void 0 : _e.parent) === null || _f === void 0 ? void 0 : _f.arguments[0]) === null || _g === void 0 ? void 0 : _g.body) === null || _h === void 0 ? void 0 : _h.callee) === null || _j === void 0 ? void 0 : _j.name;
-                    // Is it starts with use (ex. useAuthStore starts with use)
-                }
-                else if (callee.startsWith('use')) {
-                    // Remove use
-                    newStoreName = callee.split('use')[1];
-                }
-                if (newStoreName && newStoreName !== '') {
-                    // First caracter should be lowercase
-                    newStoreName = newStoreName[0].toLowerCase() + newStoreName.substring(1);
-                    // Report and suggest a fix
-                    context.report({
-                        node: node,
-                        messageId: 'useCorrectStoreName',
-                        data: {
-                            correctStoreName: newStoreName,
-                        },
-                        suggest: [
-                            {
-                                desc: `Change to ${newStoreName}`,
-                                fix: function (fixer) {
-                                    return fixer.replaceText(node, newStoreName);
-                                }
+                if (callee && typeof callee === 'string') {
+                    if (callee === 'useStore') {
+                        newStoreName = (_p = (_o = (_m = (_l = (_k = (_j = (_h = (_g = (_f = node === null || node === void 0 ? void 0 : node.parent) === null || _f === void 0 ? void 0 : _f.id) === null || _g === void 0 ? void 0 : _g.parent) === null || _h === void 0 ? void 0 : _h.init) === null || _j === void 0 ? void 0 : _j.callee) === null || _k === void 0 ? void 0 : _k.parent) === null || _l === void 0 ? void 0 : _l.arguments[0]) === null || _m === void 0 ? void 0 : _m.body) === null || _o === void 0 ? void 0 : _o.callee) === null || _p === void 0 ? void 0 : _p.name;
+                        // Is it starts with use (ex. useAuthStore starts with use)
+                    }
+                    else if (callee.startsWith('use')) {
+                        // Remove use
+                        newStoreName = callee.split('use')[1];
+                    }
+                    if (newStoreName && newStoreName !== '') {
+                        // First caracter should be lowercase
+                        newStoreName = newStoreName[0].toLowerCase() + newStoreName.substring(1);
+                        // Report and suggest a fix
+                        context.report({
+                            node: node,
+                            messageId: 'useCorrectStoreName',
+                            data: {
+                                correctStoreName: newStoreName,
                             },
-                        ]
-                    });
+                            suggest: [
+                                {
+                                    desc: `Change to ${newStoreName}`,
+                                    fix: function (fixer) {
+                                        return fixer.replaceText(node, newStoreName);
+                                    }
+                                },
+                            ]
+                        });
+                    }
                 }
             }
         }
--- src/correct-store-name/index.ts
+++ src/correct-store-name/index.ts
@@ -22,39 +22,41 @@ const myRule: TSESLint.RuleModule<MessageIds> = {
         // ex.
         // const store = useAuthStore()
         // Triggers when a const variable name is store
-        if (node.name === 'store') {
+        if (node.name === 'store' && 'parent' in node && 'id' in node.parent && 'parent' in node.parent.id && 'init' in node.parent.id.parent && 'callee' in node.parent.id.parent.init) {
             // Get the name of the value we are trying to assign to store
-            const callee = node.parent.id.parent.init.callee.name
+            const callee = node?.parent?.id?.parent?.init?.callee?.name
             let newStoreName: any = ''
             // If we are initializing a new store
-            if (callee === 'useStore') {
-                newStoreName = node?.parent?.id?.parent?.init?.callee?.parent?.arguments[0]?.body?.callee?.name
-            // Is it starts with use (ex. useAuthStore starts with use)
-            } else if (callee.startsWith('use')) {
-                // Remove use
-                newStoreName = callee.split('use')[1]
-            }
-            
-            if (newStoreName && newStoreName !== '') {   
-                // First caracter should be lowercase
-                newStoreName = newStoreName[0].toLowerCase() + newStoreName.substring(1)
-
-                // Report and suggest a fix
-                context.report({
-                    node: node,
-                    messageId: 'useCorrectStoreName',
-                    data: {
-                        correctStoreName: newStoreName,
-                    },
-                    suggest: [
-                        {
-                            desc: `Change to ${newStoreName}`,
-                            fix: function (fixer: any) {
-                                return fixer.replaceText(node, newStoreName);
-                            }
+            if (callee && typeof callee === 'string') {
+                if (callee === 'useStore') {
+                    newStoreName = node?.parent?.id?.parent?.init?.callee?.parent?.arguments[0]?.body?.callee?.name
+                // Is it starts with use (ex. useAuthStore starts with use)
+                } else if (callee.startsWith('use')) {
+                    // Remove use
+                    newStoreName = callee.split('use')[1]
+                }
+                
+                if (newStoreName && newStoreName !== '') {   
+                    // First caracter should be lowercase
+                    newStoreName = newStoreName[0].toLowerCase() + newStoreName.substring(1)
+    
+                    // Report and suggest a fix
+                    context.report({
+                        node: node,
+                        messageId: 'useCorrectStoreName',
+                        data: {
+                            correctStoreName: newStoreName,
                         },
-                    ]
-                });
+                        suggest: [
+                            {
+                                desc: `Change to ${newStoreName}`,
+                                fix: function (fixer: any) {
+                                    return fixer.replaceText(node, newStoreName);
+                                }
+                            },
+                        ]
+                    });
+                }
             }
         }
     }
